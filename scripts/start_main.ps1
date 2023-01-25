b4000670783bdafa03cd68e8ecc3cb3648000a50
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-26 01:25:32 +0900

    削除

--- scripts/start_main.ps1
+++
@@ -1,6 +0,0 @@
-pushd "%~dp0.."
-call .venv\Scripts\activate.bat
-uvicorn --host 0.0.0.0 --app-dir backend app.main:app
-
-@REM uvicorn --reload --host 0.0.0.0 --app-dir backend app.main:app
-@REM gunicorn app.main:app --pythonpath backend --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
