fc8d2b76bd40eef6b179dd192e386df9fda0cfa2
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-28 16:29:53 +0900

    検索フィールド追加

--- frontend/src/routes/links/+page.svelte
+++ frontend/src/routes/links/+page.svelte
@@ -7,32 +7,51 @@
 		TableBodyCell,
 		TableBodyRow,
 		TableHead,
-		TableHeadCell
+		TableHeadCell,
+		TableSearch
 	} from 'flowbite-svelte';
 	import type { PageServerData } from './$types';
 
+	let searchTerm = '';
 	export let data: PageServerData;
+
+	$: filterdItems =
+		searchTerm.length > 0
+			? data.links.filter(
+					(link) =>
+						link?.attributes?.title?.includes(searchTerm) ||
+						link?.attributes?.url?.includes(searchTerm) ||
+						link?.attributes?.description?.includes(searchTerm)
+			  )
+			: data.links;
 </script>
 
 <div class="w-full">
 	{#if data.links.length > 0}
-		<Table striped={true}>
+		<TableSearch
+			striped={true}
+			placeholder="Search keywords..."
+			hoverable={true}
+			bind:inputValue={searchTerm}
+		>
 			<TableHead>
 				<TableHeadCell>Title</TableHeadCell>
 				<TableHeadCell>Description</TableHeadCell>
 			</TableHead>
-			<TableBody>
-				{#each data.links as link (link?.id)}
+			<TableBody tableBodyClass="divide-y">
+				{#each filterdItems as link (link?.id)}
 					<TableBodyRow>
 						<TableBodyCell
 							><A target="_blank" href={link?.attributes?.url}>{link?.attributes?.title}</A
 							></TableBodyCell
 						>
-						<TableBodyCell>{link?.attributes?.description}</TableBodyCell>
+						<TableBodyCell tdClass="px-6 py-4 lg:whitespace-nowrap"
+							>{link?.attributes?.description}</TableBodyCell
+						>
 					</TableBodyRow>
 				{/each}
 			</TableBody>
-		</Table>
+		</TableSearch>
 	{:else}
 		<P class="w-full">データがありません</P>
 	{/if}
