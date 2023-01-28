1f0bb4708684cdcbd9268b6991393b44f41e9384
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-28 15:09:17 +0900

    エラーチェックを追加

--- frontend/src/routes/links/+page.server.ts
+++ frontend/src/routes/links/+page.server.ts
@@ -14,9 +14,12 @@ export const load = (async ({ fetch }): Promise<IPageFetchData> => {
 	try {
 		const res = await fetch(strapiUrl(`links`));
 		const json = await res.json();
-		console.log(json);
-		const links = json.data as ILink[];
+		if (json.error) {
+			console.log(json);
+			throw new Error(json.error.message);
+		}
 
+		const links = json.data as ILink[];
 		return { links };
 	} catch (err) {
 		console.log(err);
