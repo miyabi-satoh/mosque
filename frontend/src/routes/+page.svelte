dcede5a501af1a8a92f7392f77449186fb3192c1
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-28 16:29:11 +0900

    Cardコンポーネント採用

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import { Heading, P, Span } from 'flowbite-svelte';
+	import { Card, Heading, P, Span } from 'flowbite-svelte';
 	import type { LayoutServerData } from './$types';
 
 	export let data: LayoutServerData;
@@ -8,10 +8,7 @@
 
 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
 	{#each data.menuItems as menuItem (menuItem?.id)}
-		<a
-			href={menuItem?.attributes?.url}
-			class="flex flex-col h-64 bg-white rounded-lg border border-gray-100 hover:border-white dark:border-gray-700 dark:hover:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light dark:bg-gray-900"
-		>
+		<Card padding="none" href={menuItem?.attributes?.url}>
 			<div
 				class="w-full bg-gray-50 dark:bg-gray-700 rounded-t-md py-2.5 px-5 flex justify-between items-center border-b border-gray-200 dark:border-gray-700"
 			>
@@ -25,6 +22,6 @@
 					{menuItem?.attributes?.description}
 				</P>
 			</div>
-		</a>
+		</Card>
 	{/each}
 </div>
