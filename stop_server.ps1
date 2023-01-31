e20a77cfb56366943c59baaff7344686b64a39fc
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-31 23:05:56 +0900

    winsw stop

--- stop_server.ps1
+++ stop_server.ps1
@@ -1,23 +1,6 @@
 #!/usr/bin/env pwsh
 
-$pid_array = @()
-
-if ($IsWindows) {
-  $pid_array = Get-CimInstance -Class Win32_Process |
-  Where-Object { $_.CommandLine -like "*gunicorn*" } |
-  Select-Object ProcessId
-}
-else {
-  foreach ($line in ps aux | grep python | grep uvicorn) {
-    $pid_array = $pid_array + (-split $line)[1]
-  }
-}
-
-foreach ($id in $pid_array) {
-  Write-Host "kill $id"
-  Stop-Process -Id $id
-}
-
-
 Set-Location $PSScriptRoot
+
+./winsw stop mosque.xml
 docker compose -f compose.yml -f compose-prod.yml down
