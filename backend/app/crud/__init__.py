de35f801ed1813f170e0ad19a6e8890b1a830cfb
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-31 15:23:47 +0900

    未使用モジュールをコメントアウト

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,7 +1,7 @@
 # from .crud_item import item
-from .crud_user import user  # noqa
-from .crud_page import page  # noqa
-from .crud_link import link  # noqa
+# from .crud_user import user  # noqa
+# from .crud_page import page  # noqa
+# from .crud_link import link  # noqa
 from .crud_format import format  # noqa
 
 # For a new basic set of CRUD operations you could just do
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
-from .msg import Msg  # noqa
-from .token import Token, TokenPayload  # noqa
-from .user import User, UserCreate, UserRead, UserUpdate  # noqa
-from .page import Page, PageCreate, PageRead, PageUpdate  # noqa
-from .link import Link, LinkCreate, LinkRead, LinkUpdate  # noqa
+# from .msg import Msg  # noqa
+# from .token import Token, TokenPayload  # noqa
+# from .user import User, UserCreate, UserRead, UserUpdate  # noqa
+# from .page import Page, PageCreate, PageRead, PageUpdate  # noqa
+# from .link import Link, LinkCreate, LinkRead, LinkUpdate  # noqa
 from .format import Formats, FormatCreate, FormatRead, FormatUpdate  # noqa
