fe121a1934004a8b05ce28c18e8067c5990f6513
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-03 01:43:54 +0900

    import周りのeslintプラグイン導入

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['svelte3', '@typescript-eslint', 'unused-imports', 'import'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -16,5 +16,19 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'@typescript-eslint/no-unused-vars': 'off', // or "no-unused-vars"
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+		],
+		'import/order': [
+			'error',
+			{
+				groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index']
+			}
+		]
 	}
 };
