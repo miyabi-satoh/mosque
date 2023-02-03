ed04d994d432aac90f701476bef49f156816b191
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-04 00:34:09 +0900

    絶対パスに変更

--- frontend/src/lib/api/utils.ts
+++ frontend/src/lib/api/utils.ts
@@ -1,9 +1,9 @@
 export type Fetch = typeof fetch;
 
 export function apiUrl(url: string) {
-	return `/api/v1/${url}`;
+	return `http://localhost/api/v1/${url}`;
 }
 
 export function strapiUrl(url: string) {
-	return `/strapi/${url}`;
+	return `http://localhost/strapi/${url}`;
 }
