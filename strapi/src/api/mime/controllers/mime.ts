f74ab78886dab638e785f4ac89688362b79dd423
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-06 01:15:54 +0900

    Mimeを削除

--- strapi/src/api/mime/controllers/mime.ts
+++
@@ -1,7 +0,0 @@
-/**
- * mime controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::mime.mime');
--- strapi/src/api/mime/services/mime.ts
+++
@@ -1,7 +0,0 @@
-/**
- * mime service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::mime.mime');
