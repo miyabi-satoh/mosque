9d84c2167760f5ac0defdcfdedb63319b291d14c
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-13 16:07:22 +0900

    daisyUIへ移行中

--- frontend/src/routes/footer.svelte
+++ frontend/src/routes/footer.svelte
@@ -1,16 +1,8 @@
-<script lang="ts">
-	import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from 'flowbite-svelte';
-</script>
-
-<div class="lg:pl-64 border-t border-gray-200 dark:border-gray-700 mt-6 lg:mt-8">
-	<Footer footerType="custom" customClass="py-6 lg:py-8 px-4 lg:px-8 dark:bg-gray-900">
-		<div class="sm:flex sm:items-center sm:justify-between">
-			<FooterCopyright href="/" by="miyabi-satoh" year={2023} />
-			<FooterLinkGroup
-				ulClass="flex flex-wrap items-center mt-3 sm:mt-0 text-sm text-gray-500 dark:text-gray-400"
-			>
-				<FooterLink href="/about">About</FooterLink>
-			</FooterLinkGroup>
+<div class="lg:pl-64 border-t mt-6">
+	<div class="footer py-6 px-4">
+		<div>© 2023 miyabi-satoh . All Rights Reserved.</div>
+		<div class="justify-self-end">
+			<a class="link" href="/about">About</a>
 		</div>
-	</Footer>
+	</div>
 </div>
