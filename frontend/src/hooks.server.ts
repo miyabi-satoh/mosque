79988a07a7a9fd8c5514efbccf4d11637e0b9ec8
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-18 00:10:04 +0900

    localhostを環境変数に

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,8 +1,9 @@
 import type { HandleFetch } from '@sveltejs/kit';
+import { PUBLIC_SERVER_NAME } from '$env/static/public';
 
 export const handleFetch = (async ({ request, fetch }) => {
-	if (request.url.startsWith('http://localhost:')) {
-		const newUrl = request.url.replace('http://localhost:', 'http://127.0.0.1:');
+	if (request.url.startsWith(`http://${PUBLIC_SERVER_NAME}`)) {
+		const newUrl = request.url.replace(`http://${PUBLIC_SERVER_NAME}`, 'http://127.0.0.1');
 		console.log(request.url);
 		console.log(` -> ${newUrl}`);
 		// clone the original request, but change the URL
