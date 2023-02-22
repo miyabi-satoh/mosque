b425a93edffa1dbcdb6ef370c00c2306a2c33525
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-22 13:15:36 +0900

    更新

--- frontend/src/lib/Breadcrumb.svelte
+++ frontend/src/lib/Breadcrumb.svelte
@@ -11,21 +11,23 @@
 	export let params: BreadcrumbParamType[];
 </script>
 
-<div class="text-sm breadcrumbs not-prose mb-8">
-	<ul>
-		{#each params as { href, name }, index}
-			<li>
-				{#if index == params.length - 1}
-					{name}
-				{:else}
-					<a {href} class="link link-hover">
-						{#if href == '/'}
-							<Icon icon="mdi:home" class="mr-2" />
-						{/if}
+{#if params.length > 0}
+	<div class="text-sm breadcrumbs not-prose mb-8">
+		<ul>
+			{#each params as { href, name }, index}
+				<li>
+					{#if index == params.length - 1}
 						{name}
-					</a>
-				{/if}
-			</li>
-		{/each}
-	</ul>
-</div>
+					{:else}
+						<a {href} class="link link-hover">
+							{#if href == '/'}
+								<Icon icon="mdi:home" class="mr-2" />
+							{/if}
+							{name}
+						</a>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</div>
+{/if}
