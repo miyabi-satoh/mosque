ac4769eefd5d5e7597cb76422e338fab23436a19
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-22 11:26:09 +0900

    更新

--- frontend/src/lib/utils.vitest.ts
+++ frontend/src/lib/utils.vitest.ts
@@ -1,13 +1,24 @@
 import { it, expect, describe } from 'vitest';
 import { formatDate, normalizeSearch } from './utils';
 
-describe('utilsのテスト', () => {
-	it('formatDateのテスト', () => {
-		const result = formatDate('2023-02-12');
-
-		expect(result).toBe('2023年2月12日(日)');
+describe('formatDateのテスト', () => {
+	it('date == null => 空文字列', () => {
+		const result = formatDate(null);
+		expect(result).toBe('');
 	});
 
+	it('date == "2023-02-22" => 2023年2月22日(水)', () => {
+		const result = formatDate('2023-02-22');
+		expect(result).toBe('2023年2月22日(水)');
+	});
+
+	it('date == Date(2023-02-22) => 2023年2月22日(水)', () => {
+		const result = formatDate(new Date('2023-02-22'));
+		expect(result).toBe('2023年2月22日(水)');
+	});
+});
+
+describe('utilsのテスト', () => {
 	it('normalizeSearchのテスト', () => {
 		let result = normalizeSearch('１２３４');
 		expect(result).toBe('1234');
