9a637f76effa9a101a64fcb5878bf3287aafea3e
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-23 19:16:24 +0900

    __dirnameのエラー対策

--- frontend/src/lib/server/prisma.ts
+++ frontend/src/lib/server/prisma.ts
@@ -1,3 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+// https://github.com/prisma/prisma/issues/5030
+import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url ?? __filename);
+
+const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
+
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
+
+class PrismaClient extends _PrismaClient {}
 
 export const prisma = new PrismaClient();
