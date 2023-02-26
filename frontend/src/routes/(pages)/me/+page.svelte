9c8fbc856f485142e3b57c8f4069ac149f40a9d7
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-27 02:53:35 +0900

    addToast

--- frontend/src/routes/(pages)/me/+page.svelte
+++ frontend/src/routes/(pages)/me/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
+	import type { ActionData } from './$types';
+	import { addToast } from '$lib/components/Toast.svelte';
 	import { userStore } from '$lib/user';
 
-	$: me = $userStore;
+	export let form: ActionData;
+	$: if (form?.message) {
+		addToast(form.message, form.user ? 'alert-success' : 'alert-error');
+		if (form.user) {
+			$userStore = form.user;
+		}
+	}
 </script>
 
-{#if !me}
+{#if !$userStore}
 	<p>このページはログインが必要です。</p>
 {:else}
 	<form method="POST">
-		<input type="hidden" name="id" value={me.id} />
 		<div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
 			<div class="flex flex-col">
 				<label for="displayName">表示名(5文字以内)</label>
@@ -18,7 +25,7 @@
 					name="displayName"
 					maxlength="5"
 					required
-					bind:value={me.displayName}
+					value={$userStore.displayName}
 				/>
 			</div>
 			<div class="flex items-end">
@@ -30,23 +37,38 @@
 			<div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
 				<div class="flex flex-col">
 					<label for="sei">姓</label>
-					<input class="input input-bordered w-full" id="sei" disabled value={me.sei} />
+					<input class="input input-bordered w-full" id="sei" disabled value={$userStore.sei} />
 				</div>
 				<div class="flex flex-col">
 					<label for="mei">名</label>
-					<input class="input input-bordered w-full" id="mei" disabled value={me.mei} />
+					<input class="input input-bordered w-full" id="mei" disabled value={$userStore.mei} />
 				</div>
 				<div class="flex flex-col">
 					<label for="seiKana">姓(カナ)</label>
-					<input class="input input-bordered w-full" id="seiKana" disabled value={me.seiKana} />
+					<input
+						class="input input-bordered w-full"
+						id="seiKana"
+						disabled
+						value={$userStore.seiKana}
+					/>
 				</div>
 				<div class="flex flex-col">
 					<label for="meiKana">名(カナ)</label>
-					<input class="input input-bordered w-full" id="meiKana" disabled value={me.meiKana} />
+					<input
+						class="input input-bordered w-full"
+						id="meiKana"
+						disabled
+						value={$userStore.meiKana}
+					/>
 				</div>
 				<div class="flex flex-col">
 					<label for="abbrev">略称</label>
-					<input class="input input-bordered w-full" id="abbrev" disabled value={me.abbrev} />
+					<input
+						class="input input-bordered w-full"
+						id="abbrev"
+						disabled
+						value={$userStore.abbrev}
+					/>
 				</div>
 			</div>
 		</div>
