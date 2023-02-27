eb9377ec3f40d900968eda5d0f43f8046db27d6f
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-27 11:37:28 +0900

    作業中

--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -2,9 +2,38 @@
 	import { error } from '@sveltejs/kit';
 	import { userStore } from '$lib/user';
 
+	const menuItems = [
+		{
+			id: 1,
+			url: '/',
+			title: 'title',
+			description: 'description'
+		}
+	];
 	$: if (!$userStore || $userStore.id != 1) {
 		error(404, 'Not Found');
 	}
 </script>
 
-<div>admin page</div>
+<div class="my-8">
+	<h1>管理ページ</h1>
+</div>
+
+<div class="my-8">
+	<h2>メニュー</h2>
+	<div class="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 not-prose">
+		{#each menuItems as menuItem (menuItem.id)}
+			<a
+				class="card card-compact border-2 border-base-200 bg-base-300/25 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
+				href={menuItem.url}
+			>
+				<div class="card-body">
+					<h3 class="card-title">
+						{menuItem.title}
+					</h3>
+					<p>{menuItem.description}</p>
+				</div>
+			</a>
+		{/each}
+	</div>
+</div>
