c47e2ebb9150f2cb9749564ff67aff447ac32882
miyabi-satoh <miyabi.satoh@gmail.com>
2023-02-28 13:01:08 +0900

    pageMetaの存在チェック

--- frontend/src/routes/(pages)/+layout.svelte
+++ frontend/src/routes/(pages)/+layout.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	// import type { LayoutData } from './$types';
 	import { page } from '$app/stores';
 	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
 
-	// export let data: LayoutData;
-	// $: pageMeta = $page.data.pageMeta;
+	console.log(`frontend/src/routes/(pages)/+layout.svelte`);
 </script>
 
 <svelte:head>
-	<title>MOSQUE | {$page.data.pageMeta.title}</title>
+	{#if $page.data.pageMeta?.title}
+		<title>MOSQUE | {$page.data.pageMeta.title}</title>
+	{/if}
 </svelte:head>
 
 <Breadcrumb params={$page.data.breadcrumbParams} />
-{#if $page.data.pageMeta.title}
+{#if $page.data.pageMeta?.title}
 	<h1 class="mt-0 mb-2">{$page.data.pageMeta.title}</h1>
 {/if}
-{#if $page.data.pageMeta.description}
+{#if $page.data.pageMeta?.description}
 	<div>{$page.data.pageMeta.description}</div>
 {/if}
 <div class="pt-8">
