8e1df87e4636e346aab9451a59288fdb609c44b6
miyabi-satoh <miyabi.satoh@gmail.com>
2023-03-04 11:38:19 +0900

    新規作成

---
+++ frontend/src/lib/components/form/IconLinkButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let href: string;
+	export let icon: string;
+	export let iconHeight = 20;
+	let className = 'btn-primary';
+	export { className as class };
+</script>
+
+<a {href} class="btn gap-2 {className}" on:click>
+	<Icon {icon} height={iconHeight} />
+	<span><slot /></span>
+</a>
