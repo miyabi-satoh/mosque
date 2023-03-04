7a1971322db7078c09676add79012048646086fe
miyabi-satoh <miyabi.satoh@gmail.com>
2023-03-04 10:44:57 +0900

    encryptPassword

--- frontend/src/lib/server/passwd.ts
+++ frontend/src/lib/server/passwd.ts
@@ -4,8 +4,7 @@ export const encryptPassword = (password: string | null | undefined) => {
 	if (password) {
 		return hashSync(password, 10);
 	}
-	console.log(`encryptPassword: ${password}`);
-	throw new Error(`Throws error from encryptPassword.`);
+	return undefined;
 };
 
 export const comparePassword = (password1: string | null, password2: string | null) => {
