3506a963f6ffdc59f96299c079aafcd540f73b7b
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-02 07:59:22 +0900

    作業中

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,42 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>
-	Visit <a class="link" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<main class="p-4">
+	<div class="grid grid-cols-1 gap-4">
+		<select class="select select-bordered w-full max-w-xs">
+			<option value="" disabled selected>学年</option>
+			<option value="7">中1</option>
+			<option value="8">中2</option>
+			<option value="9">中3</option>
+		</select>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.500);
-	}
-</style>
+		<select class="select select-bordered w-full max-w-xs">
+			<option value="" disabled selected>年度</option>
+			<option value="2023">2023</option>
+			<option value="2022">2022</option>
+			<option value="2021">2021</option>
+		</select>
+
+		<select class="select select-bordered w-full max-w-xs">
+			<option value="" disabled selected>月号</option>
+			<option value="1">1月号</option>
+			<option value="4">4月号</option>
+			<option value="7">7月号</option>
+		</select>
+	</div>
+
+	<div class="card w-96 bg-base-100 border-2 border-dotted rounded-lg">
+		<div class="card-body">
+			<h2 class="card-title">国語</h2>
+			<div class="card-actions justify-center">
+				<button class="btn btn-primary">再生</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">英語</h2>
+			<div class="card-actions justify-center">
+				<button class="btn btn-primary">再生</button>
+			</div>
+		</div>
+	</div>
+</main>
