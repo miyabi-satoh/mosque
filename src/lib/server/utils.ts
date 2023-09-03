e169d6bbcd4e2b4cbb35848e31d717f4a68c964d
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-03 22:15:08 +0900

    とりあえず動く

---
+++ src/lib/server/utils.ts
@@ -0,0 +1,28 @@
+import { CTEST_MEDIA_CSV } from '$env/static/private';
+import fs from 'node:fs';
+import { parse } from 'csv-parse/sync';
+
+type CTestMediaT = {
+	year: string;
+	grade: string;
+	month: string;
+	subj: string;
+	path: string;
+};
+
+const columnKeys = ['year', 'grade', 'month', 'subj', 'path'];
+
+export function loadCTestMediaData(): CTestMediaT[] {
+	return parse(fs.readFileSync(CTEST_MEDIA_CSV).toString(), {
+		columns: () => columnKeys
+	});
+}
+
+export function findMediaData(key: string): CTestMediaT | undefined {
+	const buffer = fs.readFileSync(CTEST_MEDIA_CSV).toString();
+	const data = buffer.split('\n').find((line) => line.startsWith(key));
+	if (data) {
+		return parse(data, { columns: columnKeys })[0];
+	}
+	return undefined;
+}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,19 @@
+import { loadCTestMediaData } from '$lib/server/utils';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-    return {};
+	const csvData = loadCTestMediaData();
+	const grades = new Map<string, string>();
+	const gradeSet = new Set<number>();
+	csvData.forEach((obj) => gradeSet.add(Number(obj.grade)));
+	[...gradeSet].sort().forEach((g) => {
+		if (g === 7 || g === 8 || g === 9) {
+			grades.set(`${g}`, `中${Number(g) - 6}`);
+		}
+	});
+
+	return {
+		csvData,
+		grades
+	};
 }) satisfies PageServerLoad;
