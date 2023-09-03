51069aae1aa372c7aa5311acbf9cd7b5711483f6
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-03 23:55:57 +0900

    ルート変更、他

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="min-h-screen flex flex-col">
+	<slot />
+	<footer class="text-sm text-base-content/50 text-right p-4">
+		Copyright &copy; 2023 miyabi-satoh.
+	</footer>
+</div>
