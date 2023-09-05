c16b2b54dec2127dcdb70375e3c078187b8ada85
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-05 23:39:56 +0900

    AudioPlayerコンポーネントに変更

--- src/routes/eiken/+page.svelte
+++ src/routes/eiken/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import type { PageData } from './$types';
-	import { onDestroy, onMount } from 'svelte';
-	import { browser } from '$app/environment';
+	import { tick } from 'svelte';
+	import { AudioPlayer } from '$lib';
 
 	export let data: PageData;
+	let audioSrc: string = '';
+	let audioTitle: string = '';
+	let audioPaused: boolean = true;
 
 	let selectedGrade = '';
 	let yearMap = new Map<string, string>();
@@ -96,143 +99,22 @@
 		return '';
 	}
 
-	let audio: HTMLAudioElement;
-	let currentTime = 0;
-	let remainTime = 0;
-	let duration = 0;
-
-	onMount(() => {
-		audio.autoplay = false;
-		audio.loop = false;
-		audio.muted = false;
-		audio.playbackRate = 1.0;
-		audio.preload = 'metadata';
-		audio.volume = 1.0;
-		audio.addEventListener('loadedmetadata', () => {
-			duration = Math.floor(audio.duration);
-			remainTime = duration;
-		});
-		audio.addEventListener('ended', () => {
-			audio.currentTime = 0;
-			pauseAudio();
-			updateTime();
-		});
-	});
-
-	function handleClickPlayPause(url: string) {
-		if (audio.src.endsWith(url)) {
-			// 再生中だったら停止
-			if (!audio.paused) {
-				pauseAudio();
-			} else {
-				playAudio();
-			}
-		} else if (url !== '') {
-			// 再生中だったら停止
-			if (!audio.paused) {
-				pauseAudio();
-			}
-			audio.src = url;
-			playAudio();
-		}
-		audio = audio;
-	}
-
-	let audioError = '';
-	let timerId = 0;
-	let seeking = false;
-
-	function onInterval() {
-		clearInterval(timerId);
-		timerId = window.setInterval(() => {
-			if (audio && !seeking) {
-				updateTime();
-			}
-		}, 500);
-	}
-
-	async function playAudio() {
-		if (audio) {
-			audioError = '';
-			try {
-				await audio.play();
-				onInterval();
-			} catch (err) {
-				if (err instanceof Error) {
-					audioError = err.message;
-				} else {
-					audioError = 'Unexpected error';
-				}
-			}
-		}
-	}
-
-	function pauseAudio() {
-		if (audio && !audio.paused) {
-			audio.pause();
-			clearInterval(timerId);
-		}
-	}
-
-	onDestroy(() => {
-		pauseAudio();
-	});
-
-	function handleClickSkipHead() {
-		audio.currentTime = 0;
-		updateTime();
-	}
-
-	function handleClickSkipBack() {
-		audio.currentTime -= 10;
-		updateTime();
-	}
-
-	function handleClickSkipForward() {
-		audio.currentTime += 10;
-		updateTime();
-	}
-
-	function handleInputRange(event: Event) {
-		seeking = true;
-		const target = event.target as HTMLInputElement;
-		currentTime = Number(target.value);
-	}
-
-	function handleChangeRange(event: Event) {
-		console.log(`handleChangeRange`);
-		const target = event.target as HTMLInputElement;
-		audio.currentTime = Number(target.value);
-		updateTime();
-		seeking = false;
-	}
-
-	function updateTime() {
-		currentTime = Math.floor(audio.currentTime);
-	}
-
-	function formatTime(sec: number) {
-		const m = Math.floor(sec / 60);
-		const s = Math.floor(sec - m * 60);
-		return `${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
-	}
-
-	$: if (browser && currentTime !== undefined) {
-		remainTime = duration - currentTime;
-		const e = window.document.getElementById('range-audio') as HTMLInputElement;
-		if (e) {
-			const min = Number(e.min);
-			const max = Number(e.max);
-			const val = Number(currentTime);
-
-			e.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%';
+	async function handleClickPlayPause(title: string, src: string) {
+		if (audioSrc === src) {
+			audioPaused = !audioPaused;
+		} else {
+			audioPaused = true;
+			await tick();
+			audioSrc = src;
+			audioTitle = title;
+			audioPaused = false;
 		}
 	}
 </script>
 
 <main class="container mx-auto max-w-3xl flex-1">
 	<h1 class="p-4 text-4xl font-semibold">英検過去問配信サービス</h1>
-	<div class="grid grid-cols-1 sm:grid-cols-3 gap-4 p-4">
+	<div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-3">
 		<select
 			id="select-grade"
 			bind:value={selectedGrade}
@@ -270,25 +152,23 @@
 		</select>
 	</div>
 
-	<div class="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
+	<div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2">
 		{#each [...mediaUrls.entries()] as [key, url]}
-			<div class="card bg-base-200 dark:bg-neutral/25 border border-base-content/20 rounded w-full">
-				<div class="card-body p-4 justify-between">
+			<div class="card w-full rounded border border-base-content/20 bg-base-200 dark:bg-neutral/25">
+				<div class="card-body justify-between p-4">
 					<h2 class="card-title">{key}</h2>
-					<div class="card-actions justify-center mt-4">
+					<div class="card-actions mt-4 justify-center">
 						{#if key.toLowerCase().includes('part')}
 							<button
-								on:click={() => handleClickPlayPause(url)}
+								on:click={() => handleClickPlayPause(key, url)}
 								disabled={!url}
-								class="btn btn-sm btn-info rounded-3xl w-16 {audio && audio.src.endsWith(url)
-									? ''
-									: 'btn-outline'}"
+								class:btn-outline={!audioSrc.endsWith(url)}
+								class="btn btn-info btn-sm w-16 rounded-3xl"
 							>
-								{#if audio && audio.src.endsWith(url) && !audio.paused}
-									<Icon icon="mdi:pause" height="auto" />
-								{:else}
-									<Icon icon="mdi:play" height="auto" />
-								{/if}
+								<Icon
+									height="auto"
+									icon={audioSrc.endsWith(url) && !audioPaused ? 'mdi:pause' : 'mdi:play'}
+								/>
 							</button>
 						{:else}
 							<a href={url} target="_blank" class="btn btn-primary">新しいタブで開く</a>
@@ -299,95 +179,5 @@
 		{/each}
 	</div>
 
-	<div class="p-4">
-		{#if audioError}
-			<div class="alert alert-error">
-				<Icon icon="mdi:close-circle-outline" height="auto" />
-				<span>{audioError}</span>
-			</div>
-		{/if}
-	</div>
-
-	<audio bind:this={audio} />
-	{#if audio && audio.src}
-		<div class="flex p-4 gap-4 items-center">
-			<button class="btn btn-square btn-ghost" on:click={handleClickSkipHead}>
-				<Icon icon="mdi:skip-previous" height="48px" />
-			</button>
-			<button class="btn btn-square btn-ghost" on:click={handleClickSkipBack}>
-				<Icon icon="fluent:skip-back-10-32-regular" height="32px" />
-			</button>
-			<button
-				on:click={() => handleClickPlayPause(audio.src)}
-				class="btn btn-circle btn-info h-16 w-16"
-			>
-				{#if !audio.paused}
-					<Icon icon="mdi:pause" height="64px" />
-				{:else}
-					<Icon icon="mdi:play" height="64px" />
-				{/if}
-			</button>
-			<button class="btn btn-square btn-ghost" on:click={handleClickSkipForward}>
-				<Icon icon="fluent:skip-forward-10-32-regular" height="32px" />
-			</button>
-			<div class="flex flex-col flex-1">
-				<div class="flex justify-between">
-					<span>{formatTime(currentTime)}</span>
-					<span>{formatTime(remainTime)}</span>
-				</div>
-				<input
-					id="range-audio"
-					type="range"
-					value={currentTime}
-					on:input={handleInputRange}
-					on:change={handleChangeRange}
-					min="0"
-					max={duration}
-					class="w-full"
-				/>
-			</div>
-		</div>
-	{/if}
+	<AudioPlayer src={audioSrc} title={audioTitle} bind:paused={audioPaused} />
 </main>
-
-<style>
-	input[type='range'] {
-		appearance: none;
-		height: 8px;
-		border-radius: 5px;
-		background: #0f172a;
-		background-image: linear-gradient(#38bdf8, #38bdf8);
-		background-size: 0% 100%;
-		box-shadow: 0px 1px 1px rgba(255, 255, 255, 0.06);
-		background-repeat: no-repeat;
-	}
-
-	input[type='range']::-webkit-slider-thumb,
-	input[type='range']::-moz-range-thumb,
-	input[type='range']::-ms-thumb {
-		-webkit-appearance: none;
-		appearance: none;
-		-moz-appearance: none;
-		height: 24px;
-		width: 24px;
-		border-radius: 50%;
-		background: #e2e8f0;
-		cursor: ew-resize;
-		box-shadow:
-			0px 1px 3px rgba(0, 0, 0, 0.1),
-			0px 1px 2px rgba(0, 0, 0, 0.06);
-		-webkit-transition: background 0.3s ease-in-out;
-		transition: background 0.3s ease-in-out;
-	}
-
-	input[type='range']::-webkit-slider-runnable-track,
-	input[type='range']::-moz-range-track,
-	input[type='range']::-ms-track {
-		-webkit-appearance: none;
-		appearance: none;
-		-moz-appearance: none;
-		box-shadow: none;
-		border: none;
-		background: transparent;
-	}
-</style>
