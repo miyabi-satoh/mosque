7672b839f1afbd052511c43c02c19494d9fdf3ba
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-24 22:49:35 +0900

    sessionExpiresInの設定値を.env経由に

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -2,14 +2,21 @@ import { prisma } from '@lucia-auth/adapter-prisma';
 import { lucia } from 'lucia';
 import { sveltekit } from 'lucia/middleware';
 
-import { dev } from '$app/environment';
+import { ACTIVE_PERIOD_MINUTES, IDLE_PERIOD_MINUTES } from '$env/static/private';
 
 import { db } from '$lib/server/db';
 
+// import { dev } from '$app/environment';
+
 export const auth = lucia({
-	env: dev ? 'DEV' : 'PROD',
+	// env: dev ? 'DEV' : 'PROD',
+	env: 'DEV',
 	middleware: sveltekit(),
 	adapter: prisma(db),
+	sessionExpiresIn: {
+		activePeriod: 1000 * 60 * Number(ACTIVE_PERIOD_MINUTES),
+		idlePeriod: 1000 * 60 * Number(IDLE_PERIOD_MINUTES)
+	},
 
 	getUserAttributes: (data) => {
 		return {
