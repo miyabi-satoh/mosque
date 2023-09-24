7d2aed776c122acd4e12199a28265d5a20da1e53
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-24 23:06:29 +0900

    メニュー表示をシンプルに

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,17 +1,44 @@
+import { URLS } from '$lib/consts';
 import { db } from '$lib/server/db';
 
 import type { PageServerLoad } from './$types';
 
+type MenuT = {
+	href: string;
+	label: string;
+};
 export const load: PageServerLoad = async () => {
+	const menus: MenuT[] = [];
 	const exam = await db.exam.findMany({
 		orderBy: { sortOrder: 'asc' }
 	});
+	menus.push(
+		...exam.map((e) => {
+			return {
+				href: `${URLS.ADMIN}/${e.examType}`,
+				label: `${e.shortName}`
+			} satisfies MenuT;
+		})
+	);
+	menus.push(
+		{
+			href: URLS.ADMIN_SITELINK,
+			label: `外部リンク`
+		},
+		{
+			href: ``,
+			label: `ユーザ`
+		},
+		{
+			href: ``,
+			label: `スタッフ`
+		}
+	);
 
-	const siteLinks = await db.siteLink.findMany({
-		orderBy: { sortOrder: 'asc' }
-	});
+	// const siteLinks = await db.siteLink.findMany({
+	// 	orderBy: { sortOrder: 'asc' }
+	// });
 	return {
-		exam,
-		siteLinks
+		menus
 	};
 };
