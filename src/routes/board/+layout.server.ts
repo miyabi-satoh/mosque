dba0e223a4e79c8aba196409902e92a7ba471cd0
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-25 00:54:37 +0900

    リダイレクト処理を修正

--- src/routes/board/+layout.server.ts
+++ src/routes/board/+layout.server.ts
@@ -1,14 +1,18 @@
+import { redirect } from '@sveltejs/kit';
+
 import { URLS } from '$lib/consts';
 import { hasStaffRole } from '$lib/utils';
 
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async ({ locals }) => {
-	// ログインセッションを取得
-	const session = await locals.auth.validate();
-	// 認可
-	if (!session || !hasStaffRole(session.user)) {
-		return Response.redirect(URLS.BOARD_AUTH, 302);
+export const load = (async ({ locals, url }) => {
+	if (url.pathname !== URLS.BOARD_AUTH) {
+		// ログインセッションを取得
+		const session = await locals.auth.validate();
+		// 認可
+		if (!session || !hasStaffRole(session.user)) {
+			throw redirect(302, URLS.BOARD_AUTH);
+		}
 	}
 
 	return {};
