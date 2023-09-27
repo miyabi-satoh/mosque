b1b3b1d975efdb62eba64b498977aa4810a3a107
miyabi-satoh <miyabi.satoh@gmail.com>
2023-09-27 15:18:20 +0900

    class(classes)プロパティを追加

--- src/lib/components/HelperText.svelte
+++ src/lib/components/HelperText.svelte
@@ -1,3 +1,8 @@
-<div class="text-error-500-400-token text-sm">
+<script lang="ts">
+	let classes = '';
+	export { classes as class };
+</script>
+
+<div class="text-error-500-400-token text-sm {classes}">
 	<slot />
 </div>
