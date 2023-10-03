355500c44a696fd7e83a30070ba48ca7eb0a8fcf
miyabi-satoh <miyabi.satoh@gmail.com>
2023-10-04 03:02:28 +0900

    handleFetch

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,7 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+
+import { ORIGIN, PORT } from '$env/static/private';
 
 import { auth } from '$lib/server/lucia';
 
@@ -10,3 +12,12 @@ const authHandler: Handle = async ({ event, resolve }) => {
 };
 
 export const handle = sequence(authHandler);
+
+export const handleFetch: HandleFetch = async ({ request, fetch }) => {
+	if (request.url.startsWith(ORIGIN)) {
+		// clone the original request, but change the URL
+		request = new Request(request.url.replace(ORIGIN, `http://localhost:${PORT}/`), request);
+	}
+
+	return fetch(request);
+};
