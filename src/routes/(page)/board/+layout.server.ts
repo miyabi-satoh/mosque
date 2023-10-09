49adaeb0b00737c2ae3828b5f1ee27c267739713
miyabi-satoh <miyabi.satoh@gmail.com>
2023-10-09 17:23:31 +0900

    change

--- src/routes/(page)/board/+layout.server.ts
+++ src/routes/(page)/board/+layout.server.ts
@@ -1,12 +1,13 @@
 import { error } from '@sveltejs/kit';
 
 import { URLS } from '$lib/consts';
+import { isWindows } from '$lib/utils';
 
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async ({ parent }) => {
+export const load = (async ({ request, parent }) => {
 	const data = await parent();
-	if (data.user || data.isWindows) {
+	if (data.user || isWindows(request.headers.get('User-Agent'))) {
 		data.breadcrumbs.push({ label: 'Board', link: URLS.BOARD });
 
 		return {
