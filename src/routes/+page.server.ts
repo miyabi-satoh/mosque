b7844bfc69cc43a7bd0f7bc9dba3dbdfa45ad552
miyabi-satoh <miyabi.satoh@gmail.com>
2023-10-12 17:57:40 +0900

    remove fail on logout action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { fail, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 import { db } from '$lib/server/db';
 import { auth } from '$lib/server/lucia';
@@ -43,9 +43,10 @@ export const load = (async () => {
 export const actions: Actions = {
 	logout: async ({ locals }) => {
 		const session = await locals.auth.validate();
-		if (!session) return fail(401);
-		await auth.invalidateSession(session.sessionId); // invalidate session
-		await auth.deleteDeadUserSessions(session.user.userId);
+		if (session) {
+			await auth.invalidateSession(session.sessionId); // invalidate session
+			await auth.deleteDeadUserSessions(session.user.userId);
+		}
 		locals.auth.setSession(null); // remove cookie
 		throw redirect(302, '/'); // redirect to root page
 	}
