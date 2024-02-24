2825f6b18154b33db7d6df6a74472ec1cdac3fa9
miyabi-satoh <miyabi.satoh@gmail.com>
2024-02-24 04:23:27 +0000

    作業中

--- src/routes/(page)/(loggedIn)/admin/archives/[id]/items/+page.server.ts
+++ src/routes/(page)/(loggedIn)/admin/archives/[id]/items/+page.server.ts
@@ -2,34 +2,92 @@ import { URLS } from '$lib/consts';
 import { db } from '$lib/server/db';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { readdirSync } from 'node:fs';
+import fs from 'node:fs';
 import path from 'node:path';
-import { ArchiveItemSchema } from '$lib/schemas/zod';
+import type { ArchiveItem } from '@prisma/client';
 
-function searchFiles(dirPath: string): string[] {
-  const allDirents = readdirSync(dirPath, { withFileTypes: true });
+function searchFiles(dirPath: string, re: RegExp): string[] {
+  const allDirents = fs.readdirSync(dirPath, { withFileTypes: true });
 
   const files: string[] = [];
   for (const dirent of allDirents) {
     if (dirent.isDirectory()) {
       const subDirPath = path.join(dirPath, dirent.name);
-      files.push(...searchFiles(subDirPath));
+      files.push(...searchFiles(subDirPath, re));
     } else if (dirent.isFile()) {
-      files.push(path.join(dirPath, dirent.name));
+      if (dirent.name.toLowerCase().match(re)) {
+        files.push(path.join(dirPath, dirent.name));
+      }
     }
   }
 
   return files;
 }
 
-const schema = ArchiveItemSchema.pick({
-  strGrade: true,
-  strSequence: true,
-  strYear: true,
-  title: true
-}).extend({
-  id: ArchiveItemSchema.shape.id.optional()
-})
+function getItemProps(itemPath: string) {
+  // todo: パスから各種プロパティを取得
+  const props: Omit<ArchiveItem, 'id' | 'archiveId'> = {
+    title: path.basename(itemPath),
+    state: 'deny',
+    path: itemPath,
+    year: null,
+    strYear: null,
+    grade: null,
+    strGrade: null,
+    sequence: null,
+    strSequence: null,
+  }
+  const yearMatch = itemPath.match(/(\d{4})(年|年度)/);
+  if (yearMatch) {
+    props.year = parseInt(yearMatch[1]);
+    props.strYear = yearMatch[0]
+  }
+  const gradeMatch = itemPath.match(/(小|中|高|準)(\d)級?/);
+  if (gradeMatch) {
+    props.grade = parseInt(gradeMatch[2]);
+    props.strGrade = gradeMatch[0];
+    if (gradeMatch[1] === '中') {
+      props.grade += 6;
+    }
+    else if (gradeMatch[1] === '高') {
+      props.grade += 9;
+    }
+    else if (gradeMatch[1] === '準') {
+      props.grade = props.grade * 10 + 5
+    }
+    else if (gradeMatch[0].endsWith('級')) {
+      props.grade *= 10
+    }
+  }
+  const sequenceMatch = itemPath.match(/第?(\d{1,2})(回|月|月号)/);
+  if (sequenceMatch) {
+    props.sequence = parseInt(sequenceMatch[1]);
+    props.strSequence = sequenceMatch[0];
+  }
+
+  if (props.title.endsWith('.mp3')) {
+    if (props.strSequence?.includes('月')) {
+      if (props.title.includes('国語')) {
+        props.title = '国語 聞き取り問題'
+      }
+      else if (props.title.includes('英語')) {
+        props.title = '英語 リスニング問題'
+      }
+    }
+  }
+  else if (props.strGrade?.match(/準|級/)) {
+    const m = props.title.match(/^(p?)(\d)q-?part(?<part>\d).mp3$/i);
+    if (m && m.groups) {
+      const part = Number(m.groups.part);
+      props.title = `リスニング音源(Part${part})`;
+    }
+
+  }
+
+
+  return props
+}
+
 
 export const load = (async ({ parent, params }) => {
   const data = await parent();
@@ -43,21 +101,30 @@ export const load = (async ({ parent, params }) => {
   })
   if (!archive) error(404, 'Archive not found.');
 
-  const diskItems = searchFiles(archive.path)
+  const re = /\.(mp3|pdf)$/
+  const diskItems = searchFiles(archive.path, re)
   try {
-    for (const path of diskItems) {
+    for (const diskItem of diskItems) {
+      const itemPath = diskItem.replace(archive.path, '')
       const item = await db.archiveItem.findUnique({
         where: {
           archiveId_path: {
             archiveId: archive.id,
-            path
+            path: itemPath
           }
         }
       })
       if (!item) {
-        // todo: パスから各種プロパティを取得
+        await db.archiveItem.create({
+          data: {
+            ...getItemProps(itemPath),
+            archiveId: archive.id
+          }
+        })
       }
     }
+  } catch (e) {
+    console.error(e)
   }
 
   const items = Array.from(new Set([
