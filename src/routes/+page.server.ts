1aff56ca4334c7ca53dc8ea8815838bef55f43c9
miyabi-satoh <miyabi.satoh@gmail.com>
2024-02-27 01:42:30 +0000

    整理

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,38 +6,20 @@ import { deleteSessionCookie, invalidateSession } from '$lib/server/lucia';
 
 import type { Actions, PageServerLoad } from './$types';
 
-type ItemT = {
-	href: string;
-	title: string;
-	external: boolean;
-};
-
-export const load = (async () => {
-	// const exams = await db.exam.findMany({
-	// 	orderBy: { sortOrder: 'asc' }
-	// });
-
-	const links = await db.link.findMany({
-		orderBy: [{ sortOrder: 'desc' }, { title: 'asc' }]
-	});
-
+export const load: PageServerLoad = async () => {
+	// アーカイブリストを取得します
 	const archives = await db.archive.findMany({
 		where: { items: { some: { published: true } } },
 		orderBy: [{ sortOrder: 'asc' }, { title: 'asc' }]
 	});
-	const items: ItemT[] = [
-		// ...exams.map((exam) => {
-		// 	return {
-		// 		href: `/${exam.examType}`,
-		// 		title: `${exam.name}アーカイブ`,
-		// 		external: false
-		// 	};
-		// }),
-		// {
-		// 	href: '/archives',
-		// 	title: 'その他のアーカイブ',
-		// 	external: false
-		// },
+
+	// 外部リンクリストを取得します
+	const links = await db.link.findMany({
+		orderBy: [{ sortOrder: 'desc' }, { title: 'asc' }]
+	});
+
+	// 取得したリストを配列にします
+	const items = [
 		...archives.map((archive) => {
 			return {
 				href: URLS.ARCHIVES(archive.path),
@@ -54,20 +36,14 @@ export const load = (async () => {
 		})
 	];
 
-	return {
-		items
-	};
-}) satisfies PageServerLoad;
+	return { items };
+};
 
 export const actions: Actions = {
+	// ログアウト
 	logout: async ({ locals, cookies }) => {
 		await invalidateSession(locals.session);
 		deleteSessionCookie(cookies);
-		// const sessionCookie = lucia.createBlankSessionCookie();
-		// cookies.set(sessionCookie.name, sessionCookie.value, {
-		// 	path: '.',
-		// 	...sessionCookie.attributes
-		// });
 		redirect(302, '/'); // redirect to root page
 	}
 };
